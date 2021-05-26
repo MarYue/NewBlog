eec4e368288355c21c4254cdcfa916f019f69d22
马玥 <mayue@eastmoney.com>
2021-05-26 17:31:21 +0800

    fix: 异步vuex

--- blog/vuex/store.js
+++ blog/vuex/store.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-return-assign */
 import Vue from 'vue'
 import Vuex from 'vuex'
 
@@ -7,27 +8,49 @@ const state = {
   count: 1
 }
 
+// 利用 mutations 同步修改状态
 // 改变 state 的数值的方法，必须写在 mutation 中
 const mutations = {
   // 可以传参数
   add (state, n) {
     state.count += n
   },
-  reduce (state) {
-    state.count--
+  reduce (state, n = 1) {
+    state.count -= n
   }
 }
 
 // getters 可以看作在获取数据前进行的一种再编辑，相当于对数据的一种过滤和加工。可以看作 store.js 的计算属性
 const getters = {
   count: function (state) {
-    let res = state.count
-    res += 5
-    state.count = res
-    return state.count
+    return state.count += 5
   }
 }
 
+// 利用 actions 异步修改状态
+// actions 可以调用 mutations 里的方法
+const actions = {
+  // context 上下文对象，可以理解称 store 本身
+  addAction (context) {
+    context.commit('add', 5)
+    setTimeout(() => {
+      context.commit('reduce')
+    }, 3000)
+    console.log('我比reduce提前执行')
+  },
+  // {commit} 直接把commit对象传递过来，让方法体逻辑和代码更清晰明了
+  reduceAction ({commit}) {
+    commit('reduce')
+  }
+}
+
+// 用模块组的方式，分组后按组编写
+const moduleA = {
+  state, mutations, getters, actions
+}
+
 export default new Vuex.Store({
-  state, mutations, getters
+  state, mutations, getters, actions
 })
+
+export { moduleA }
